dc3cb011362995bee49b572880ed0a81e4c0e341
Vítězslav Procházka <vitezslav.prochazka@student.gyarab.cz>
2023-05-25 23:00:37 +0200

    User_camera_formating
    formated the code

--- SRC/GUI/UserCamera.gd
+++ SRC/GUI/UserCamera.gd
@@ -1,22 +1,20 @@
 extends Camera2D
+
 #value of how zoom will be aplied
 var zoom_value = Vector2(0.1,0.1)
 var zoom_max = Vector2 (2,2)
 var zoom_min = Vector2 (0.1,0.1)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	
 func _process(delta):
 	if (Input.is_action_just_released("zoom_in")):
 		zoom_in()
 	if (Input.is_action_just_released("zoom_out")):
 		zoom_out()
-	print(zoom)
+#will zoom camera in (makes things look bigger)
 func zoom_in():
 	if (zoom <= zoom_max):
 		zoom += zoom_value
+#will zoom camera out (makes things look smaller)
 func zoom_out():
 	if (zoom >= zoom_min):
 		zoom -= zoom_value
