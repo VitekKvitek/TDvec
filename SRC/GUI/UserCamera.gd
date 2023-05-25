d99645e654a7c701114457e81ef407fe09df012f
Vítězslav Procházka <vitezslav.prochazka@student.gyarab.cz>
2023-05-25 22:55:22 +0200

    camera_zoom_status_ok
    Made camera zoom okay working

--- SRC/GUI/UserCamera.gd
+++ SRC/GUI/UserCamera.gd
@@ -1,19 +1,23 @@
 extends Camera2D
 #value of how zoom will be aplied
 var zoom_value = Vector2(0.1,0.1)
+var zoom_max = Vector2 (2,2)
+var zoom_min = Vector2 (0.1,0.1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
+	
 func _process(delta):
 	if (Input.is_action_just_released("zoom_in")):
 		zoom_in()
 	if (Input.is_action_just_released("zoom_out")):
 		zoom_out()
-
+	print(zoom)
 func zoom_in():
-	zoom += zoom_value
+	if (zoom <= zoom_max):
+		zoom += zoom_value
 func zoom_out():
-	zoom -= zoom_value
+	if (zoom >= zoom_min):
+		zoom -= zoom_value
 	
