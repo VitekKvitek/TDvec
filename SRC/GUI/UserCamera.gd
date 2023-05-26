16310341af3215b2a1ee76e2d85c499558cf2874
Vítězslav Procházka <95168347+VitekKvitek@users.noreply.github.com>
2023-05-26 11:21:28 +0200

    bg

--- SRC/GUI/UserCamera.gd
+++ SRC/GUI/UserCamera.gd
@@ -2,10 +2,20 @@ extends Camera2D
 
 #value of how zoom will be aplied
 var zoom_value = Vector2(0.1,0.1)
-var zoom_max = Vector2 (2,2)
-var zoom_min = Vector2 (0.1,0.1)
+var zoom_max = Vector2 (4,4)
+var zoom_min = Vector2 (0.3,0.3)
+
+var camera_movement_speed = Vector2 (100,100)
 
 func _process(delta):
+	if (Input.is_action_pressed("camera_move_up")):
+		move_up()
+	if (Input.is_action_pressed("camera_move_down")):
+		move_down()
+	if (Input.is_action_pressed("camera_move_right")):
+		move_right()
+	if (Input.is_action_pressed("camera_move_left")):
+		move_left()
 	if (Input.is_action_just_released("zoom_in")):
 		zoom_in()
 	if (Input.is_action_just_released("zoom_out")):
@@ -18,4 +28,11 @@ func zoom_in():
 func zoom_out():
 	if (zoom >= zoom_min):
 		zoom -= zoom_value
-	
+func move_up():
+	transform.y += camera_movement_speed
+func move_down():
+	transform.y -= camera_movement_speed
+func move_right():
+	transform.x += camera_movement_speed
+func move_left():
+	transform.x -= camera_movement_speed
